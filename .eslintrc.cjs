469370705e155206cd71e256766ad84994a867da
myungsujang <myungsujang89@gmail.com>
2024-07-11 21:03:14 +0900

    fix: eslintrc 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,28 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: [
-    'react-refresh',
-    '@typescript-eslint',
-    'prettier',
-  ],
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'prettier/prettier': [
-      'error', {
-          'endOfLine': 'auto',
-          'tabWidht': 'auto',
-      }
+      'error',
+      {
+        endOfLine: 'auto',
+        tabWidht: 'auto',
+      },
     ],
   },
   overrides: [
     {
       files: ['*.ts?(x)'],
       parser: '@typescript-eslint/parser',
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-      ],
+      extends: ['plugin:@typescript-eslint/recommended'],
       rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-      }
+      },
     },
   ],
-}
+};
